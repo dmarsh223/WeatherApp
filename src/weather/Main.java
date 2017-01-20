291f129ca4f0f345581962630bfc88f850d73745
dmarsh223 <danmarsh223@gmail.com>
2017-01-20 15:05:20 -0500

    updated with oracle code. reads in XML and prints out XML

--- src/weather/Main.java
+++ src/weather/Main.java
@@ -5,7 +5,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-    XMLReader.Reader();
+//    XMLReader.Reader();
+    XMLReader.parseWeather("http://api.wunderground.com/api/184245b00544774c/conditions/q/NJ/Manahawkin.xml");
+
+//    XMLReader.parseXml2("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nucleotide&id=224589801");
+
+
+
+
 
     }
 }
--- src/weather/XMLReader.java
+++ src/weather/XMLReader.java
@@ -1,42 +1,50 @@
 package weather;
 
-//import java.net.MalformedURLException;
-//import java.util.Scanner;
-//import java.net.URL;
+
+import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
 import java.net.*;
-import java.io.*;
+
 
 
 public class XMLReader {
 
 
-        public static void Reader() throws IOException {
+    public static void parseWeather(String URL) {
+        DOMParser parser = new DOMParser();
 
-        URL oracle = null;
         try {
-            oracle = new URL("http://api.wunderground.com/api/184245b00544774c/conditions/q/NJ/Manahawkin.xml");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        BufferedReader in = new BufferedReader(
-                    new InputStreamReader(oracle.openStream()));
+            parser.parse(new InputSource(new URL(URL).openStream()));
 
-            String inputLine;
-        try {
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            //example found here http://stackoverflow.com/questions/24446849/parse-xml-without-tagname
+
+            Document doc = parser.getDocument();
+            Node weatherInfo = doc.getElementsByTagName("display_location").item(0);
+            NodeList children = weatherInfo.getChildNodes();
+
+            for (int i = 0; i < children.getLength(); i++) {
+                if (children.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element elem = (Element) children.item(i);
+                    // If your document is namespace aware use localName
+                    //String localName = elem.getLocalName();
+                    // Tag name returns the localName and the namespace prefix
+                    String tagName = elem.getTagName();
+                    String itemInfo = elem.getTextContent();
+                    System.out.println(tagName + " " + itemInfo);
+                    // do stuff with the children
+                }
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
 
 }
+
+
 
 
 
